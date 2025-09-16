d246d904402ddae26ec380a14e1edeaff65e83dd
mzyrkowski <marcinzyrkowski7@gmail.com>
2025-09-16 16:54:46 +0200

    added more utils

---
+++ src/main/java/org/example/utils/EnumUtils.java
@@ -0,0 +1,20 @@
+package org.example.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EnumUtils {
+
+  public <T extends Enum<T>> T getRandomValue(Class<T> enumClass) {
+    T[] values = enumClass.getEnumConstants();
+
+    if (values == null || values.length == 0) {
+      throw new IllegalArgumentException("Enum class must have at least one constant");
+    }
+
+    int randomIndex = BookerRandomUtils.RANDOM.randomInt(0, values.length);
+    return values[randomIndex];
+  }
+
+}
